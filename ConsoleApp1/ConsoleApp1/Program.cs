bf2749326ff4625409c8681e22d96d5181975d30
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-11-30 19:24:50 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Unesite cijeli broj: ");
+                int broj = int.Parse(Console.ReadLine());
+                if (broj == 0)
+                {
+                    break;
+                }
+                brojevi.Add(broj);
+            }
+            brojevi.Sort();
+            brojevi.Reverse();
+            Console.WriteLine("Uneseni brojevi pad. redosljedom: ");
+            foreach (int broj in brojevi)
+            {
+                Console.WriteLine("{0}, ", broj);
+            }
+            Console.ReadKey();
+        }
+    }
+}
